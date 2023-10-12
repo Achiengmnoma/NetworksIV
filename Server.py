573f1f78a9f99ee612345428da9dc5514e11561f
Daniel Niven <2481553@dundee.ac.uk>
2023-10-12 04:21:51 +0100

    updated logic

--- Server.py
+++ Server.py
@@ -242,30 +242,23 @@ class Server:
                     print(f"{addr_header_send} Message sent to {target}")
                 
                 elif command == "SLAP":
-                    if len(words) >= 2:  # Ensure that there are at least 2 elements in the words list
-                        # Get a list of users in the channel
-                        channel_name = words[1].strip()  # Assuming the channel name is the argument after SLAP
-                        channel_users = this.channels.get(channel_name, [])
-
-                        # Exclude the user who sent the command from the list of potential targets
-                        potential_targets = [user['nick'] for user in channel_users if user['nick'] != user_details['nick']]
-
-                        if potential_targets:
-                            # Choose a random target from the list
-                            target = random.choice(potential_targets)
+                    # Ensure that the SLAP command is properly formatted
+                    if len(words) >= 1:
+                        # Choose a random user from the list of users (excluding the bot itself)
+                        users_to_slap = [user['nick'] for user in this.users if user['nick'] != user_details['nick']]
+        
+                        if users_to_slap:
+                            target = random.choice(users_to_slap)
 
                             # Send the slap message to the channel
                             slap_message = f":{user_details['nick']}!{user_details['username']}@{addr[0]} PRIVMSG {'#Bot_Commands'} :{target}, you've been slapped!\r\n"
                             this.Send(slap_message.encode('ascii'))
-                    
+
                             # Log the action in the server
                             print(f"{addr_header_send} {user_details['nick']} slapped {target}")
                         else:
                             # Inform the user that there's no one to slap
-                            user_details['user'].send(f":{user_details['hostname']} NOTICE {user_details['nick']} :No one to slap in {channel_name}\r\n".encode('ascii'))
-                    else:
-                        # Inform the user that the SLAP command is not properly formatted
-                        user_details['user'].send(f":{user_details['hostname']} NOTICE {user_details['nick']} :SLAP command should be formatted as SLAP #channel\r\n".encode('ascii'))
+                            user_details['user'].send(f":{user_details['hostname']} NOTICE {user_details['nick']} :No one to slap\r\n".encode('ascii'))
                 elif command == 'TOPIC':
                     print(f"User tryed to change the topic")
               
